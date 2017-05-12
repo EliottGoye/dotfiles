9f6b00ac0cb562a2bc4461c15675668f346bd1e6
Eliott Goyé <eliott.goye@smartpanda.com>
2017-05-12 10:17:56 +0200

    add(bootsrap): powerline fonts & apt softs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+apt_install() {
+  echo "Installing soft..."
+  sudo apt-get update
+  sudo apt-get install htop vim mtr locate curl zsh
+}
+
 git_install() {
   which git &> /dev/null
   if [[ $? -ne 0 ]]; then
     echo "Git install..."
-    sudo apt-get update
     sudo apt-get install git -y
   fi
 
   echo "Dotfiles update..."
   git pull origin master
+}
+
+powerline_fonts_install() {
+  git clone https://github.com/powerline/fonts.git
+  fonts/install.sh
+  rm -rf fonts
 }
 
 link_dotfiles() {
@@ -20,7 +31,9 @@ link_dotfiles() {
 }
 
 main() {
+  apt_install
   git_install
+  powerline_fonts_install
   link_dotfiles
 }
 
