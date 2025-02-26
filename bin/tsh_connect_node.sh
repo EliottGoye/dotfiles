042a39c0cdb7a0f6e8643e4ff0f183a446a8fe2e
Eliott Goyé <eliott.goye@docaposte.fr>
2025-02-26 15:01:38 +0100

    feat: add server name to tmux tab on tsh connect

--- bin/tsh_connect_node.sh
+++ bin/tsh_connect_node.sh
@@ -5,7 +5,9 @@ NODE=$(tsh ls | fzf --header-lines=2 | cut -d" " -f1)
 if [ -z "$NODE" ]; then
     echo "No node chosen. Exiting..."
 else
+    tmux rename-window -t "${TMUX_PANE}" "$NODE"
     tsh ssh "root@$NODE"
+    tmux rename-window -t "${TMUX_PANE}" "local"
 fi
 
 exit
